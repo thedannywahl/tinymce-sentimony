8f41f5e1af6ccfd8ae9492bc3795f5daa3620afc
Danny Wahl <danny@iywahl.com>
2019-02-13 20:56:38 -0700

    directly insert UI

--- src/plugin.js
+++ src/plugin.js
@@ -7,69 +7,59 @@ let sentimony = new Sentimony()
 let globalData = {}
 
 const plugin = editor => {
-  //let globalData = {}
-  let sentimonyUI = {
-    type: 'label',
-    name: 'sentimony',
-    html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
-    classes: 'sentimony',
-    disabled: editor.settings.readonly,
-    onclick: function() {
-      editor.windowManager.open({
-        title: 'Sentimony',
-        buttons: [
-          {
-            text: 'OK',
-            onClick: 'close'
-          }
-        ],
-        body: [
-          {
-            type : 'listbox',
-            name : 'categories',
-            label : 'Select a category',
-            onselect: function(e) {
-              let selectedSection = this.$el[0].innerText
-              sentimony.showReport(selectedSection, globalData)
-            },
-            values : [
-              { text: 'Overview', value: 'overview', selected: true },
-              { text: 'Score', value: 'Score'}
-            ]
-          },
-          {
-            type: 'container',
-            html: '<hr style="border-bottom:1px inset;">'
-          },
-          {
-            type: 'container',
-            layout: 'flow',
-            minWidth: 400,
-            minHeight: 160,
-            id: 'sentimony-report',
-            items: [
-              {
-                type: 'label',
-                text: sentimony.showReport("Overview", globalData)
-              }
-            ]
-          }
-        ]
-      })
-    }
-  }
-
   editor.on('init', function() {
     tinymce.DOM.loadCSS('./src/plugin.css')
-    let statusbar =
-      editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
-    if (statusbar) statusbar.insert(sentimonyUI, 0)
+    let statusbar = editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
+    if (statusbar) statusbar.insert({
+      type: 'label',
+      name: 'sentimony',
+      html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
+      classes: 'sentimony',
+      disabled: editor.settings.readonly,
+      onclick: function() {
+        editor.windowManager.open({
+          title: 'Sentimony',
+          buttons: [{text: 'OK', onClick: 'close'}],
+          body: [
+            {
+              type : 'listbox',
+              name : 'categories',
+              label : 'Select a category',
+              onselect: function(e) {
+                let selectedSection = this.$el[0].innerText
+                sentimony.showReport(selectedSection, globalData)
+              },
+              values : [
+                {text: 'Overview', value: 'overview', selected: true},
+                {text: 'Score', value: 'Score'}
+              ]
+            },
+            {
+              type: 'container',
+              html: '<hr style="border-bottom:1px inset;">'
+            },
+            {
+              type: 'container',
+              layout: 'flow',
+              minWidth: 400,
+              minHeight: 160,
+              id: 'sentimony-report',
+              items: [
+                {
+                  type: 'label',
+                  text: sentimony.showReport("Overview", globalData)
+                }
+              ]
+            }
+          ]
+        })
+      }
+    }, 0)
   })
 
   editor.on('Change', function(e) {
-    sentiment.analyze(editor.getContent({ format:'text'}),{},function(i,data) {
+    sentiment.analyze(editor.getContent({format:'text'}), {}, function(i,data) {
       globalData = data
-      console.log('sentiment:', globalData)
       sentimony.setComparativeEmotion(globalData, emotions)
     })
   })
