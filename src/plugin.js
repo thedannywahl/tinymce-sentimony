54d76fb9435d7667e10152924486d78d2cecf2c2
Danny Wahl <danny@iywahl.com>
2019-02-13 22:16:25 -0700

    stringify
    add helper function s() and json strings.

--- src/plugin.js
+++ src/plugin.js
@@ -4,76 +4,82 @@ let sentiment = new Sentiment()
 import emotions from './modules/emotions.json'
 import Sentimony from './modules/sentimony'
 let sentimony = new Sentimony()
+import strs from './langs/sentimony.json'
+let lang = tinymce.util.I18n.getCode()
+function s(st) {
+  return strs[lang][st]
+}
 let globalData = {}
 
 const plugin = editor => {
-  editor.on('init', function() {
     tinymce.DOM.loadCSS('./src/plugin.css')
-    let statusbar = editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
-    if (statusbar) statusbar.insert({
-      type: 'label',
-      name: 'sentimony',
-      html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
-      classes: 'sentimony',
-      disabled: editor.settings.readonly,
-      onclick: function() {
-        editor.windowManager.open({
-          title: 'Sentimony',
-          buttons: [{text: 'OK', onClick: 'close'}],
-          body: [
-            {
-              type : 'listbox',
-              name : 'categories',
-              label : 'Select a category',
-              onselect: function(e) {
-                let selectedSection = this.$el[0].innerText
-                sentimony.showReport(selectedSection, globalData)
+
+    editor.on('init', function() {
+      let statusbar = editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
+      if (statusbar) statusbar.insert({
+        type: 'label',
+        name: 'sentimony',
+        html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
+        classes: 'sentimony',
+        disabled: editor.settings.readonly,
+        onclick: function() {
+          editor.windowManager.open({
+            title: s("title"),
+            buttons: [{text: s("ok"), onClick: 'close'}],
+            body: [
+              {
+                type : 'listbox',
+                name : 'categories',
+                label : s("select a category"),
+                onselect: function(e) {
+                  let selectedSection = this.$el[0].innerText
+                  sentimony.showReport(selectedSection, globalData)
+                },
+                values : [
+                  {text: s("overview"), value: s("overview"), selected: true},
+                  {text: s("score"), value: s("score")}
+                ]
               },
-              values : [
-                {text: 'Overview', value: 'overview', selected: true},
-                {text: 'Score', value: 'Score'}
-              ]
-            },
-            {
-              type: 'container',
-              html: '<hr style="border-bottom:1px inset;">'
-            },
-            {
-              type: 'container',
-              layout: 'flow',
-              minWidth: 400,
-              minHeight: 160,
-              id: 'sentimony-report',
-              items: [
-                {
-                  type: 'label',
-                  html: sentimony.showReport("Overview", globalData)
-                }
-              ]
-            }
-          ]
-        })
-      }
-    }, 0)
-  })
-
-  editor.on('Change', function(e) {
-    sentiment.analyze(editor.getContent({format:'text'}), {}, function(i,data) {
-      globalData = data
-      sentimony.setComparativeEmotion(globalData, emotions)
+              {
+                type: 'container',
+                html: '<hr style="border-bottom:1px inset;">'
+              },
+              {
+                type: 'container',
+                layout: 'flow',
+                minWidth: 400,
+                minHeight: 160,
+                id: 'sentimony-report',
+                items: [
+                  {
+                    type: 'label',
+                    html: sentimony.showReport(s("overview"), globalData)
+                  }
+                ]
+              }
+            ]
+          })
+        }
+      }, 0)
     })
-  })
 
-  return {
-    getMetadata: function() {
-      return  {
-        name: "Sentimony",
-        url: "https://github.com/thedannywahl/sentimony/",
-        author: "Danny Wahl",
-        version: "0.1"
+    editor.on('Change', function(e) {
+      sentiment.analyze(editor.getContent({format:'text'}), {}, function(i,data) {
+        globalData = data
+        sentimony.setComparativeEmotion(globalData, emotions)
+      })
+    })
+
+    return {
+      getMetadata: function() {
+        return  {
+          name: s("title"),
+          url: "https://github.com/thedannywahl/sentimony/",
+          author: "Danny Wahl",
+          version: "0.1"
+        }
       }
     }
-  }
 }
 
 export default plugin
