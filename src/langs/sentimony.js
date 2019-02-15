8d49986cf1cb510d406784a765bca9c97b3cfb9d
Danny Wahl <danny@iywahl.com>
2019-02-15 11:12:14 -0700

    multi lang support

--- src/langs/sentimony.js
+++ src/langs/sentimony.js
@@ -3,18 +3,26 @@ var Langs = function(options) {
 };
 
 Langs.prototype.strings = {
-  "en": {
-    "title": "Sentimony",
-    "ok": "OK",
-    "select a category": "Select a category",
-    "overview": "Overview",
-    "score": "Score",
-    "overview report": "Overall, readers might see your sentiment as"
+  en: {
+    title: function() {return "Sentimony"},
+    ok: function () {return "OK"},
+    selectCategory: function() {return "Select a category"},
+    overview: function() {return "Sentiment Overview"},
+    score: function() {return "Sentiment Details"},
+    report: function(data) {
+      let sentiment = "neutral"
+      if (data.score > 0) sentiment = "positive"
+      if (data.score < 0) sentiment = "negative"
+      return `<p>Overall, readers might see your sentiment as ${sentiment}
+        because you used words like <span class="keyword">${data.negative[0]}</span>
+        ${data.negative[1] ? `and <span class="keyword">${data.negative[1]}</span>` : ''}.</p>`
+    }
   }
 }
 
-Langs.prototype.t = function(l, s) {
-  return this.strings[l][s]
+Langs.prototype.t = function(l = 'en', s, d) {
+  if(this.strings[l] === undefined) l = 'en'
+  return this.strings[l][s](d)
 }
 
 module.exports = Langs;
--- src/plugin.js
+++ src/plugin.js
@@ -10,68 +10,66 @@ let l = tinymce.util.I18n.getCode()
 let globalData = {}
 
 const plugin = editor => {
-  if(l == "en") {
-    tinymce.DOM.loadCSS('./src/plugin.css')
-    editor.on('init', function() {
-      let statusbar = editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
-      if (statusbar) statusbar.insert({
-        type: 'label',
-        name: 'sentimony',
-        html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
-        classes: 'sentimony',
-        disabled: editor.settings.readonly,
-        onclick: function() {
-          editor.windowManager.open({
-            title: s.t(l, "title"),
-            buttons: [{text: s.t(l, "ok"), onClick: 'close'}],
-            width: 425,
-            height: 250,
-            layout: 'flex',
-            body: [
-              {
-                type : 'listbox',
-                name : 'categories',
-                label : s.t(l, "selectCategory"),
-                onselect: function(e) {
-                  let selectedSection = this.$el[0].innerText
-                  sentimony.showReport(selectedSection, globalData)
-                },
-                values : [
-                  {text: s.t(l, "overview"), value: s.t(l, "overview"), selected: true},
-                  {text: s.t(l, "score"), value: s.t(l, "score")}
-                ]
+  tinymce.DOM.loadCSS('./src/plugin.css')
+  editor.on('init', function() {
+    let statusbar = editor.theme.panel && editor.theme.panel.find('#statusbar')[0]
+    if (statusbar) statusbar.insert({
+      type: 'label',
+      name: 'sentimony',
+      html: '<a id="sentimony-emotion" href="#" role="img" aria-label="" hidden></a>',
+      classes: 'sentimony',
+      disabled: editor.settings.readonly,
+      onclick: function() {
+        editor.windowManager.open({
+          title: s.t(l, "title"),
+          buttons: [{text: s.t(l, "ok"), onClick: 'close'}],
+          width: 425,
+          height: 250,
+          layout: 'flex',
+          body: [
+            {
+              type : 'listbox',
+              name : 'categories',
+              label : s.t(l, "selectCategory"),
+              onselect: function(e) {
+                let selectedSection = this.$el[0].innerText
+                sentimony.showReport(selectedSection, globalData)
               },
-              {
-                type: 'container',
-                html: '<hr style="border-bottom:1px inset;">'
-              },
-              {
-                type: 'container',
-                id: 'sentimony-report',
-                html: sentimony.showReport(s.t(l, "overview"), globalData),
-              }
-            ]
-          },)
-        }
-      }, 0)
-    })
+              values : [
+                {text: s.t(l, "overview"), value: s.t(l, "overview"), selected: true},
+                {text: s.t(l, "score"), value: s.t(l, "score")}
+              ]
+            },
+            {
+              type: 'container',
+              html: '<hr style="border-bottom:1px inset;">'
+            },
+            {
+              type: 'container',
+              id: 'sentimony-report',
+              html: sentimony.showReport(s.t(l, "overview"), globalData),
+            }
+          ]
+        },)
+      }
+    }, 0)
+  })
 
-    editor.on('Change', function(e) {
-      sentiment.analyze(editor.getContent({format:'text'}), {}, function(i,data) {
-        globalData = data
-        sentimony.setComparativeEmotion(globalData, emotions)
-        console.log(globalData)
-      })
+  editor.on('Change', function(e) {
+    sentiment.analyze(editor.getContent({format:'text'}), {}, function(i,data) {
+      globalData = data
+      sentimony.setComparativeEmotion(globalData, emotions)
+      console.log(globalData)
     })
+  })
 
-    return {
-      getMetadata: function() {
-        return  {
-          name: s.t(l, "title"),
-          url: "https://github.com/thedannywahl/sentimony/",
-          author: "Danny Wahl",
-          version: "0.1"
-        }
+  return {
+    getMetadata: function() {
+      return  {
+        name: s.t(l, "title"),
+        url: "https://github.com/thedannywahl/sentimony/",
+        author: "Danny Wahl",
+        version: "0.1"
       }
     }
   }
